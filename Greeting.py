b9c2599448d1ee57c388d5ecc3dd9efe01de6f75
RespectedAi <mabiodun611@gmail.com>
2025-03-11 22:33:28 +0100

    Modified Commit

--- Greeting.py
+++ Greeting.py
@@ -1,4 +1,5 @@
 print("Welcome to ClS world ")
 name = input("Please input your name:")
 print(f"Hello,{name}! Welcome to the CLI greet App")
+# Modified Commit
 
--- play.py
+++ play.py
@@ -1,6 +1,17 @@
+# This is just a test code to learn python
 print("Hello World")
 print("Enter your name:")
 num = 30
 print(num)
-language = "Python","Dart","Web","23","45.01"
-print(language)
+print("Next Session")
+
+language = ("Python","Dart","Web","23","45.01")
+print(language[1])
+print(language[4])
+site = "Zeeland"
+print(site)
+important = "Money","Life","God"
+print(important)
+
+capital_city = {"Kenya": "Nairobi", "Nigeria": "Lagos"}
+print(capital_city)
